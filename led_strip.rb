b235028c1ba447a86f146361a2f8cf93a30fc3b1
Gerrit Visscher <gerrit@visscher.de>
2015-02-12 22:00:55 +0100

    Add logger

--- led_strip.rb
+++ led_strip.rb
@@ -5,8 +5,28 @@ require 'pi_piper'
 include PiPiper
 
 require './strip.rb'
+require './logger.rb'
 
 DEBUG = true
+
+logger = Logger.new
+logger.debug = DEBUG
+logger.filename = 'motion.log'
+logger.log 'Starting up'
+
+# Trap ^C 
+Signal.trap('INT') {
+  logger.log 'Shutting down'
+  logger.write_log
+  exit
+}
+ 
+# Trap `Kill `
+Signal.trap('TERM') {
+  logger.log 'Shutting down'
+  logger.write_log
+  exit
+}
 
 strip = Strip.new
 strip.debug = DEBUG
@@ -15,6 +35,7 @@ pin_right = PiPiper::Pin.new(:pin => 22, :direction => :in)
 pin_left = PiPiper::Pin.new(:pin => 23, :direction => :in)
 
 after :pin => 23, :goes => :high do
+  logger.log('Motion detected: RIGHT')
   strip.on(Strip::DIRECTION_RIGHT)
   while pin_right.on?
   	strip.on(Strip::DIRECTION_RIGHT)
@@ -23,6 +44,7 @@ after :pin => 23, :goes => :high do
 end
 
 after :pin => 24, :goes => :high do
+  logger.log('Motion detected: LEFT')
   strip.on(Strip::DIRECTION_LEFT)
   while pin_left.on?
   	strip.on(Strip::DIRECTION_LEFT)
