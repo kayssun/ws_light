882b10dee8579d9d2728a1f53cfd9c0af27ebab2
Gerrit Visscher <gerrit@visscher.de>
2015-04-05 00:08:50 +0200

    [FEATURE] Add FadeAnimation

---
+++ lib/ws_light/animation/fade_animation.rb
@@ -0,0 +1,31 @@
+require 'ws_light/animation/animation_base'
+
+module WSLight
+  module Animation
+    # Slides from one set to another from left to right (obviously depending on the hardware setup)
+    class FadeAnimation < AnimationBase
+      FADE_DURATION = 50
+
+      def frames
+        FADE_DURATION + 1
+      end
+
+      def frame(count)
+        set = []
+
+        @set_from.next_frame
+        @set_to.next_frame
+
+        @set_from.full_length.times do |i|
+          set << @set_from.pixel(i).mix(@set_to.pixel(i), count.to_f/FADE_DURATION.to_f)
+        end
+
+        set
+      end
+
+      def frames_per_second
+        nil
+      end
+    end
+  end
+end
