5fec43247ca29a4064a5bfa662236ea5f3a09284
Gerrit Visscher <gerrit@visscher.de>
2020-01-01 13:39:54 +0100

    Improve rain set

--- lib/ws_light/set/weather/rain_set.rb
+++ lib/ws_light/set/weather/rain_set.rb
@@ -4,14 +4,15 @@ module WSLight
   module Set
     # Creates a set with all random colors
     class RainSet < ColorSet
-      FRAMES_PER_DROP = 24
+      FRAMES_PER_DROP = 20
       VISIBLE_DROPS = 35
-      CLOUD = Color.new(8, 8, 8)
+      CLOUD = Color.new(4, 4, 4)
 
       def init
-        @drops = (0..(@full_length - 4)).to_a.shuffle
+        @drops = (0..(@full_length - 1)).to_a.shuffle
         @max = @drops.size
-        @drops += @drops[@drops.size - VISIBLE_DROPS, VISIBLE_DROPS]
+        # append the start at the end to ensure same result window when near the % @max
+        @drops += @drops[0, VISIBLE_DROPS]
         generate_frame
       end
 
@@ -27,18 +28,17 @@ module WSLight
       end
 
       def generate_frame
-        @set = []
-        @full_length.times { @set << CLOUD }
+        @set = [CLOUD] * @full_length
         start = (@frame_count * VISIBLE_DROPS / FRAMES_PER_DROP) % @max
         VISIBLE_DROPS.times do |i|
           drop_ratio = (FRAMES_PER_DROP.to_f / VISIBLE_DROPS.to_f)
-          draw_star(@drops[start + i], @frame_count - ((start - VISIBLE_DROPS + 1 + i) * drop_ratio).to_i)
+          draw_drop(@drops[start + i], @frame_count - ((start - VISIBLE_DROPS + 1 + i) * drop_ratio).to_i)
         end
       end
 
-      def draw_star(position, star_frame)
-        blue = brightness((star_frame.to_f - (FRAMES_PER_DROP.to_f / 2.0)).abs)
-        @set[position] = Color.new(8, 8, 8 + blue)
+      def draw_drop(position, drop_frame)
+        blue = brightness(drop_frame.to_f) % 250
+        @set[position] = Color.new(4, 4, 4 + blue)
       end
 
       def pixel(number)
@@ -46,7 +46,7 @@ module WSLight
       end
 
       def brightness(frame_distance)
-        (248 * (1.0 - (frame_distance.to_f / (FRAMES_PER_DROP.to_f / 2.0)))).to_i
+        (250 * (1.0 - (frame_distance.to_f / FRAMES_PER_DROP.to_f))).to_i
       end
     end
   end
